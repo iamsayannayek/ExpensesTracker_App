bdcecd0107c7d132436b3177f2b2a842ddae1019
Sayan Nayek <sayannayek1912@gmail.com>
2026-01-06 18:29:24 +0530

    Functionality added into the Expenses Summary file

---
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,17 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const ExpensesSummary = ({ expenses, periodName }) => {
+  const expensesSum = expenses.reduce(
+    (sum, currExpense) => sum + currExpense.amount,
+    0
+  );
+  return (
+    <View>
+      <Text>{periodName}</Text>
+      <Text>Rs.{expensesSum.toFixed(2)}</Text>
+    </View>
+  );
+};
+
+export default ExpensesSummary;
